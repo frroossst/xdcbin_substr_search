124065ad81053588f6eb7a92e9c2221c1c2d9312
Adhyan H <adhyanpatel@gmail.com>
2022-11-19 20:48:30 -0600

    moved struct def to different page

---
+++ src/def_word.rs
@@ -0,0 +1,50 @@
+use std::cmp::Ordering;
+
+#[derive(Eq)]
+pub struct WordStruct
+    {
+    word: String,
+    found: Vec<u128>,
+    }
+
+impl WordStruct
+    {
+    pub fn new(word: &str) -> Self
+        {
+        WordStruct { word: String::from(word), found: Vec::new() }
+        }
+
+    pub fn add_find_location(&mut self, x: u128)
+        {
+        let _ = &self.found.push(x);
+        }
+
+    pub fn display(&self)
+        {
+        println!("word: {:?}, found: {:?}", &self.word, &self.found);
+        }
+    }
+
+impl Ord for WordStruct
+    {
+    fn cmp(&self, other: &Self) -> Ordering
+        {
+        self.word.cmp(&other.word)
+        }
+    }
+
+impl PartialOrd for WordStruct
+    {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> 
+        {
+        Some(self.cmp(other))
+        }
+    }
+
+impl PartialEq for WordStruct
+    {
+    fn eq(&self, other: &Self) -> bool 
+        {
+        self.word.to_string() == other.word.to_string()
+        }
+    }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,8 @@
 #![allow(unused_parens)]
 
-use std::{fs, io::{Write}, ops::RangeFrom, cmp::Ordering};
-
-#[derive(Eq)]
-struct WordStruct
-    {
-    word: String,
-    found: Vec<u128>,
-    }
-
-impl WordStruct
-    {
-    fn new(word: &str) -> Self
-        {
-        WordStruct { word: String::from(word), found: Vec::new() }
-        }
-
-    fn add_find_location(&mut self, x: u128)
-        {
-        let _ = &self.found.push(x);
-        }
-
-    fn display(&self)
-        {
-        println!("word: {:?}, found: {:?}", &self.word, &self.found);
-        }
-    }
-
-impl Ord for WordStruct
-    {
-    fn cmp(&self, other: &Self) -> Ordering
-        {
-        self.word.cmp(&other.word)
-        }
-    }
-
-impl PartialOrd for WordStruct
-    {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> 
-        {
-        Some(self.cmp(other))
-        }
-    }
-
-impl PartialEq for WordStruct
-    {
-    fn eq(&self, other: &Self) -> bool 
-        {
-        self.word.to_string() == other.word.to_string()
-        }
-    }
-
+use std::{fs, io::{Write}, ops::RangeFrom};
+mod def_word;
+use def_word::{WordStruct};
 
 fn main()
     {

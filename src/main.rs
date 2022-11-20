a5d6d2b7850066e5173e36ac4dd6520d9308320e
Adhyan H <adhyanpatel@gmail.com>
2022-11-19 20:37:49 -0600

    implemented Ord and cmp for Word_Struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![allow(unused_parens)]
 
-use std::{fs, io::Write, collections::BTreeMap};
-use lopdf::{Document, Object, Stream};
-use lopdf::{ObjectId, Bookmark};
+use std::{fs, io::{Write}, ops::RangeFrom, cmp::Ordering};
 
+#[derive(Eq)]
 struct WordStruct
     {
     word: String,
@@ -26,26 +25,46 @@ impl WordStruct
         {
         println!("word: {:?}, found: {:?}", &self.word, &self.found);
         }
-    
     }
+
+impl Ord for WordStruct
+    {
+    fn cmp(&self, other: &Self) -> Ordering
+        {
+        self.word.cmp(&other.word)
+        }
+    }
+
+impl PartialOrd for WordStruct
+    {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> 
+        {
+        Some(self.cmp(other))
+        }
+    }
+
+impl PartialEq for WordStruct
+    {
+    fn eq(&self, other: &Self) -> bool 
+        {
+        self.word.to_string() == other.word.to_string()
+        }
+    }
+
 
 fn main()
     {
     println!("xdcbin");
     println!("powered with <3 by Rust");
 
-    pdf_page_test();
+    let mut ws = WordStruct::new("lorem");
+    ws.add_find_location(0);
+    ws.add_find_location(1);
+    ws.add_find_location(10);
+    ws.display();
 
-    // let mut ws = WordStruct::new("lorem");
-    // ws.add_find_location(0);
-    // ws.add_find_location(1);
-    // ws.add_find_location(10);
-    // ws.display();
-
-
-    /* 
-
-    let content = fs::read_to_string(file_path).unwrap();
+    let file_path: &str = "test_cases/lorem_ipsum_100_paragraphs";
+    let content: String = fs::read_to_string(file_path).unwrap();
     let words = content.split(" ");
 
     
@@ -62,15 +81,15 @@ fn main()
             }
         }
 
+    charlen_4.sort_unstable();
+
     for j in charlen_4.iter()
         {
         println!("{:?}", j.display());
         }
 
-    // charlen_4.sort_unstable();
 
     // write_to_file(charlen_4, "charlen4.txt");
-    */
     }
 
 fn write_to_file(vec_slice: Vec<&str>, file_name: &str)
@@ -80,182 +99,4 @@ fn write_to_file(vec_slice: Vec<&str>, file_name: &str)
     let content = vec_slice.join(",");
 
     fobj.write_all(content.as_bytes()).unwrap();
-    }
-
-fn pdf_page_test()
-    {
-    let mut doc = Document::load("test_cases/wikimedia.pdf").unwrap();
-
-    let mut doc_pages: BTreeMap<ObjectId, Object> = BTreeMap::new();
-    let mut doc_objects: BTreeMap<ObjectId, Object>= BTreeMap::new();
-    // Define a starting max_id (will be used as start index for object_ids)
-    let mut max_id = 1;
-    let mut pagenum = 1;
-    // Collect all Documents Objects grouped by a map
-    let mut documents_pages = BTreeMap::new();
-    let mut documents_objects = BTreeMap::new();
-    let mut document = Document::with_version("1.5");
-
-    let mut first = false;
-    doc.renumber_objects_with(max_id);
-
-    max_id = doc.max_id + 1;
-
-    documents_pages.extend(
-        doc
-                .get_pages()
-                .into_iter()
-                .map(|(_, object_id)| {
-                    if !first {
-                        let bookmark = Bookmark::new(String::from(format!("Page_{}", pagenum)), [0.0, 0.0, 1.0], 0, object_id);
-                        document.add_bookmark(bookmark, None);
-                        first = true;
-                        pagenum += 1;
-                    }
-
-                    (
-                        object_id,
-                        doc.get_object(object_id).unwrap().to_owned(),
-                    )
-                })
-                .collect::<BTreeMap<ObjectId, Object>>(),
-    );
-    documents_objects.extend(doc.objects);
-
-    // Catalog and Pages are mandatory
-    let mut catalog_object: Option<(ObjectId, Object)> = None;
-    let mut pages_object: Option<(ObjectId, Object)> = None;
-
-    // Process all objects except "Page" type
-    for (object_id, object) in documents_objects.iter() {
-        // We have to ignore "Page" (as are processed later), "Outlines" and "Outline" objects
-        // All other objects should be collected and inserted into the main Document
-        match object.type_name().unwrap_or("") {
-            "Catalog" => {
-                // Collect a first "Catalog" object and use it for the future "Pages"
-                catalog_object = Some((
-                    if let Some((id, _)) = catalog_object {
-                        id
-                    } else {
-                        *object_id
-                    },
-                    object.clone(),
-                ));
-            }
-            "Pages" => {
-                // Collect and update a first "Pages" object and use it for the future "Catalog"
-                // We have also to merge all dictionaries of the old and the new "Pages" object
-                if let Ok(dictionary) = object.as_dict() {
-                    let mut dictionary = dictionary.clone();
-                    if let Some((_, ref object)) = pages_object {
-                        if let Ok(old_dictionary) = object.as_dict() {
-                            dictionary.extend(old_dictionary);
-                        }
-                    }
-
-                    pages_object = Some((
-                        if let Some((id, _)) = pages_object {
-                            id
-                        } else {
-                            *object_id
-                        },
-                        Object::Dictionary(dictionary),
-                    ));
-                }
-            }
-            "Page" => {}     // Ignored, processed later and separately
-            "Outlines" => {} // Ignored, not supported yet
-            "Outline" => {}  // Ignored, not supported yet
-            _ => {
-                document.objects.insert(*object_id, object.clone());
-            }
-        }
-    }
-
-    // If no "Pages" found abort
-    if pages_object.is_none() {
-        println!("Pages root not found.");
-    }
-
-    // Iter over all "Page" and collect with the parent "Pages" created before
-    for (object_id, object) in documents_pages.iter() {
-        if let Ok(dictionary) = object.as_dict() {
-            let mut dictionary = dictionary.clone();
-            dictionary.set("Parent", pages_object.as_ref().unwrap().0);
-
-            document
-                    .objects
-                    .insert(*object_id, Object::Dictionary(dictionary));
-        }
-    }
-
-    // If no "Catalog" found abort
-    if catalog_object.is_none() {
-        println!("Catalog root not found.");
-
-    }
-
-    let catalog_object = catalog_object.unwrap();
-    let pages_object = pages_object.unwrap();
-
-    // Build a new "Pages" with updated fields
-    if let Ok(dictionary) = pages_object.1.as_dict() {
-        let mut dictionary = dictionary.clone();
-
-        // Set new pages count
-        dictionary.set("Count", documents_pages.len() as u32);
-
-        // Set new "Kids" list (collected from documents pages) for "Pages"
-        dictionary.set(
-            "Kids",
-            documents_pages
-                    .into_iter()
-                    .map(|(object_id, _)| Object::Reference(object_id))
-                    .collect::<Vec<_>>(),
-        );
-
-        document
-                .objects
-                .insert(pages_object.0, Object::Dictionary(dictionary));
-    }
-       // Build a new "Catalog" with updated fields
-    if let Ok(dictionary) = catalog_object.1.as_dict() {
-        let mut dictionary = dictionary.clone();
-        dictionary.set("Pages", pages_object.0);
-        dictionary.remove(b"Outlines"); // Outlines not supported in merged PDFs
-
-        document
-                .objects
-                .insert(catalog_object.0, Object::Dictionary(dictionary));
-    }
-
-    document.trailer.set("Root", catalog_object.0);
-
-    // Update the max internal ID as wasn't updated before due to direct objects insertion
-    document.max_id = document.objects.len() as u32;
-
-    // Reorder all new Document objects
-    document.renumber_objects();
-
-     //Set any Bookmarks to the First child if they are not set to a page
-    document.adjust_zero_pages();
-
-    //Set all bookmarks to the PDF Object tree then set the Outlines to the Bookmark content map.
-    if let Some(n) = document.build_outline() {
-        if let Ok(x) = document.get_object_mut(catalog_object.0) {
-            if let Object::Dictionary(ref mut dict) = x {
-                dict.set("Outlines", Object::Reference(n));
-            }
-        }
-    }
-
-    document.compress();
-
-    // Save the merged PDF
-    // Store file in current working directory.
-    // Note: Line is exclude for when running tests
-    if false {
-        document.save("./merged.pdf").unwrap();
-    }
-
     }
